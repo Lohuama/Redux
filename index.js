0ad496af7105967ab869f5518a5563a0e50972f2
Lohuama <limalorruama98@gmail.com>
2022-12-13 11:43:48 -0300

    createStore and dispatch

---
+++ index.js
@@ -0,0 +1,26 @@
+const redux = require('redux')
+const createStore = redux.createStore;
+
+const incrementAction = (value)=>{return {type: 'INCREMENT', payload:value || 1}} 
+const decrementAction = (value)=>{return {type: 'DECREMENT', payload:value || 1}}
+
+function counterReducer(state = 6, action){
+
+    switch (action.type) {
+        case 'INCREMENT':
+                return state  + action.payload;
+        case 'DECREMENT':
+            return state  - action.payload;
+        default:
+            return state;
+    }
+
+}
+
+const store = createStore(counterReducer)
+console.log(store.getState())
+store.subscribe(()=>{console.log(store.getState())})
+
+store.dispatch(incrementAction())
+store.dispatch(incrementAction(1))
+store.dispatch(incrementAction(5))
